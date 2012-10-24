6eb43ddf9f2eadb9a3906cd57b47ee84545a1b3f
Dmitriy <dpetrov@65gb.net>
2012-10-24 20:58:03 +0400

    art sorting added

--- newapp/gen/com/example/newapp/R.java
+++ newapp/gen/com/example/newapp/R.java
@@ -18,22 +18,36 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int bg_games_test=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int bg_games1=0x7f020000;
+        public static final int bg_games2=0x7f020001;
+        public static final int bg_games3=0x7f020002;
+        public static final int bg_games4=0x7f020003;
+        public static final int bg_games_test=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_menu_name=0x7f020007;
+        public static final int o_bed_en=0x7f020008;
+        public static final int o_chair_en=0x7f020009;
+        public static final int o_socks_en=0x7f02000a;
+        public static final int o_toothbrush_en=0x7f02000b;
+        public static final int p_games_sortslots=0x7f02000c;
     }
     public static final class id {
-        public static final int Rlay=0x7f080000;
-        public static final int Rlay2=0x7f080009;
+        public static final int Rlay=0x7f08000a;
+        public static final int Rlay1=0x7f080008;
+        public static final int Rlay2=0x7f08000b;
         public static final int Rlay3=0x7f080006;
+        public static final int backlay=0x7f08000c;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
         public static final int button3=0x7f080004;
         public static final int button4=0x7f080005;
-        public static final int chr=0x7f080008;
+        public static final int chr=0x7f080009;
         public static final int game1=0x7f080001;
+        public static final int imageView1=0x7f08000d;
         public static final int index=0x7f080007;
-        public static final int menu_settings=0x7f08000a;
+        public static final int mainlay=0x7f080000;
+        public static final int menu_settings=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,8 +61,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int chr=0x7f050006;
+        public static final int g1=0x7f050007;
+        public static final int g2=0x7f050008;
+        public static final int g3=0x7f050009;
+        public static final int g4=0x7f05000a;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int nul=0x7f050005;
         public static final int puzzle=0x7f050004;
         public static final int title_activity_main=0x7f050003;
     }
--- newapp/src/com/example/newapp/PuzzleBack.java
+++ newapp/src/com/example/newapp/PuzzleBack.java
@@ -2,7 +2,6 @@ package com.example.newapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,9 +16,8 @@ import android.widget.RelativeLayout.LayoutParams;
 
 public class PuzzleBack extends Activity implements OnTouchListener, AnimationListener {
 	
-	int[] clr = new int[4];
 	LayoutParams localparams;
-	int obj_count = 4;
+	int obj_count = 3;
 	RelativeLayout myLayout;
 	RelativeLayout.LayoutParams lp;
 	int x0,y0, hiding;
@@ -40,10 +38,6 @@ public class PuzzleBack extends Activity implements OnTouchListener, AnimationLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puzzleback); 
 
-        clr[0] = Color.BLUE;
-        clr[1] = Color.GREEN;
-        clr[2] = Color.YELLOW;
-        clr[3] = Color.GRAY;
         
         half = (int) size/2;
         localBox = new ImageView(this);
@@ -61,49 +55,51 @@ public class PuzzleBack extends Activity implements OnTouchListener, AnimationLi
         	//instArr[i][0] = instx;
         	//instArr[i][1] = insty;
         	objParam[i] = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        	objParam[i].width = size;
-            objParam[i].height = size;
             objParam[i].leftMargin = instx;
         	objParam[i].topMargin = insty;
+        	
         	imageArr[i] = new ImageView(this);
-        	imageArr[i].setBackgroundColor(clr[i]);
         	imageArr[i].setId(i+1);
         	myLayout.addView(imageArr[i],objParam[i]);
         	imageArr[i].setOnTouchListener(this);
         	
         	boxParam[i] = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        	boxParam[i].width = size;
-            boxParam[i].height = size;
-            boxParam[i].leftMargin = instx;
-        	boxParam[i].topMargin = insty+500;
         	boxArr[i] = new ImageView(this);
-        	boxArr[i].setBackgroundColor(clr[i]-100);
-        	clr[i] -= 100;
         	boxArr[i].setId((i+1)*10);
         	myLayout.addView(boxArr[i],boxParam[i]);
         	
         	instx += objParam[i].width+5;
         	//insty += 51;
         }
-        clr[3] = Color.DKGRAY;
-        boxArr[3].setBackgroundColor(Color.DKGRAY);
+        imageArr[0].setBackgroundResource(R.drawable.o_toothbrush_en);
+        imageArr[1].setBackgroundResource(R.drawable.o_chair_en);
+        imageArr[2].setBackgroundResource(R.drawable.o_bed_en);
+        objParam[0].width = 70;
+        objParam[0].height = 70;
+        objParam[1].width = 90;
+        objParam[1].height = 90;
+        objParam[2].width = 105;
+        objParam[2].height = 105;
         objParam[0].leftMargin = 100;
         objParam[0].topMargin = 100;
         objParam[1].leftMargin = 205;
         objParam[1].topMargin = 100;
         objParam[2].leftMargin = 100;
         objParam[2].topMargin = 205;
-        objParam[3].leftMargin = 205;
-        objParam[3].topMargin = 205;
         
-        boxParam[0].leftMargin = 500;
-        boxParam[0].topMargin = 100;
-        boxParam[1].leftMargin = 605;
-        boxParam[1].topMargin = 100;
-        boxParam[2].leftMargin = 500;
-        boxParam[2].topMargin = 205;
-        boxParam[3].leftMargin = 605;
-        boxParam[3].topMargin = 205;
+        boxParam[0].width = 70;
+        boxParam[0].height = 70;
+        boxParam[1].width = 90;
+        boxParam[1].height = 90;
+        boxParam[2].width = 105;
+        boxParam[2].height = 105;
+        boxParam[0].leftMargin = 500-112;
+        boxParam[0].topMargin = 100+88;
+        boxParam[1].leftMargin = 605-100;
+        boxParam[1].topMargin = 100+66;
+        boxParam[2].leftMargin = 500+147;
+        boxParam[2].topMargin = 205-59;
+        
         for (int i=0; i<obj_count; i++)
         {	
         	instArr[i][0] = objParam[i].leftMargin;
@@ -145,10 +141,10 @@ public class PuzzleBack extends Activity implements OnTouchListener, AnimationLi
 	    		 y_cord = (int)event.getRawY()-y0;
 	    		 x_cord-=x_in;
 	    		 y_cord-=y_in;
-	    		 if (!((x_cord+half>localBox.getLeft())&&(x_cord+half<localBox.getRight())&&(y_cord+half>localBox.getTop())&&(y_cord+half<localBox.getBottom())))
-	    			 localBox.setBackgroundColor(clr[v.getId()-1]);
-	    		 else
-	    		 localBox.setBackgroundColor(Color.RED);
+	    		 //if (!((x_cord+half>localBox.getLeft())&&(x_cord+half<localBox.getRight())&&(y_cord+half>localBox.getTop())&&(y_cord+half<localBox.getBottom())))
+	    		 //	 localBox.setBackgroundColor(clr[v.getId()-1]);
+	    		 //else
+	    		 //localBox.setBackgroundColor(Color.RED);
 	    		 if (x_cord<x0)
 	    			 x_cord = x0;
 	    		 if (x_cord+(int)(v.getWidth())>myLayout.getWidth())
@@ -286,10 +282,10 @@ public class PuzzleBack extends Activity implements OnTouchListener, AnimationLi
     	if (x)
     	{
     	win++;
-		  if (win == 4)
-		  {
-			  startActivity(new Intent(this, Count.class));
-			  this.finish();
+		  if (win == obj_count)
+		  {myLayout.setBackgroundResource(0);
+			  PuzzleBack.this.finish();
+			  startActivity(new Intent(PuzzleBack.this, Count.class));
 			  overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
 		  }
     	}
@@ -307,4 +303,21 @@ public class PuzzleBack extends Activity implements OnTouchListener, AnimationLi
 		// TODO Auto-generated method stub
 		
 	}
+	
+	protected void onStop() {
+	    super.onStop();
+	    Log.d("LIFECYCLE", "MainActivity: onStop()");
+	    for (int i=0; i<obj_count; i++)
+	    {
+	    	imageArr[i].setImageBitmap(null);
+	    }
+	    myLayout.setBackgroundResource(0);
+	  }
+	protected void onResume() {
+	    super.onResume();
+	    Log.d("LIFECYCLE", "MainActivity: onResume()");
+	    myLayout.setBackgroundResource(R.drawable.bg_games3);
+	    
+	  }
+	
 }
